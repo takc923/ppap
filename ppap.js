78190564142e49581accf245302f6a49aa16f05f
Takeshi Kunimi <takeshi.kunimi@nextbeat.net>
2017-12-23 22:42:43 +0900

    Fix bug which happen in www.youtube.com

--- ppap.js
+++ ppap.js
@@ -1,22 +1,44 @@
-let video = document.querySelector("video");
+/**
+ * www.youtube.com is SPA.
+ * If landing page is https://www.youtube.com/ and doesn't load this script, this script will not get loaded even if move to video page.
+ * So https://www.youtube.com/ page load it.
+ * And https://www.youtube.com/ inject video tag after finish loading with delay.
+ * So try to init until succeed.
+ */
+initUntilDone();
 
-// to call frontend functions from background.js
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        return window[request.action](request.args, sender, sendResponse);
+function initUntilDone() {
+    if (!init()) {
+        setTimeout(initUntilDone, 1000);
     }
-);
+}
 
-// when video get played, register the tab and update icon
-video.addEventListener("playing", function () {
-    chrome.runtime.sendMessage({
-        action : "play",
-        args: null
+function init() {
+    let video = document.querySelector("video");
+    if (video == null) return false;
+
+    // to call frontend functions from background.js
+    chrome.runtime.onMessage.addListener(
+        function (request, sender, sendResponse) {
+            return window[request.action](request.args, sender, sendResponse);
+        }
+    );
+
+    // when video get played, register the tab and update icon
+    video.addEventListener("playing", function () {
+        chrome.runtime.sendMessage({
+            action: "play",
+            args: null
+        });
     });
-});
 
-// when video paused, update icon
-video.addEventListener("pause", callBackgroundPause);
+    // when video paused, update icon
+    video.addEventListener("pause", callBackgroundPause);
+
+    return true;
+}
+
+
 function callBackgroundPause() {
     chrome.runtime.sendMessage({
         action: "pause",
