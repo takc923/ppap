6703f1c33f70133c5ab1314e1345c27ef662b448
Takeshi Kunimi <takeshi.kunimi@umitron.com>
2019-04-13 18:43:07 +0900

    Update comments

--- ppap.js
+++ ppap.js
@@ -66,11 +66,17 @@ class Youtube extends Video {
 let video = new NullVideo(null);
 
 /**
+ * I monitor video tag every second. This is because of 2 reasons, (a) and (b) below.
+ *
+ * (a) For youtube
  * www.youtube.com is SPA.
  * If landing page is https://www.youtube.com/ and doesn't load this script, this script will not get loaded even if move to video page.
  * So https://www.youtube.com/ page load it.
  * And https://www.youtube.com/ inject video tag after finish loading with delay.
- * So check video tag every second.
+ *
+ * (b) For nicovideo
+ * A video dom at page loaded time in nicovideo will be replaced with new one.
+ * So the way that get video dom only at loaded time does not work.
  */
 
 // to call frontend functions from background.js
