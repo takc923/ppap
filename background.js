f80781d365541061cec406b1ef2ba4d1f29496e0
Takeshi Kunimi <takeshi.kunimi@umitron.com>
2019-04-13 19:04:32 +0900

    Reformat

--- background.js
+++ background.js
@@ -6,54 +6,56 @@ let targetTabId;
 
 // when a target tab get closed or move to other page, unregister the tab.
 chrome.tabs.onRemoved.addListener(function (removedTabId, removeInfo) {
-    if (removedTabId === targetTabId) unregister();
+  if (removedTabId === targetTabId) unregister();
 });
-chrome.tabs.onUpdated.addListener(function(updatedTabId, changeInfo) {
-    if (changeInfo.status === 'loading'
-        && updatedTabId === targetTabId){
-        ppapLog("tab updated. call unregister()");
-        unregister();
-        chrome.tabs.sendMessage(updatedTabId, { action: "updateLatestState" });
-    }
+chrome.tabs.onUpdated.addListener(function (updatedTabId, changeInfo) {
+  if (changeInfo.status === 'loading'
+    && updatedTabId === targetTabId) {
+    ppapLog("tab updated. call unregister()");
+    unregister();
+    chrome.tabs.sendMessage(updatedTabId, {action: "updateLatestState"});
+  }
 });
+
 function unregister(dummy, sender) {
-    targetTabId = null;
-    chrome.browserAction.setIcon({path: "icon/icon.png"});
-    chrome.browserAction.setTitle({title: "PlainPauseAndPlay"});
+  targetTabId = null;
+  chrome.browserAction.setIcon({path: "icon/icon.png"});
+  chrome.browserAction.setTitle({title: "PlainPauseAndPlay"});
 }
 
 // play or pause if you click icon or fire shortcut key
 chrome.browserAction.onClicked.addListener(toggle);
 chrome.commands.onCommand.addListener(toggle);
-function toggle() {
-    if (!targetTabId) return;
 
-    ppapLog("toggle");
-    chrome.tabs.sendMessage(
-        targetTabId,
-        { action: "toggle" },
-        null
-    );
+function toggle() {
+  if (!targetTabId) return;
+
+  ppapLog("toggle");
+  chrome.tabs.sendMessage(
+    targetTabId,
+    {action: "toggle"},
+    null
+  );
 }
 
 // to call background.js functions from frontend
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        return window[request.action](request.args, sender, sendResponse);
-    }
+  function (request, sender, sendResponse) {
+    return window[request.action](request.args, sender, sendResponse);
+  }
 );
 
 // for onMessage callback functions
-function play(args, sender){
-    ppapLog("play");
-    targetTabId = sender.tab.id;
-    chrome.browserAction.setIcon({path: iconPathNowPlaying});
-    chrome.browserAction.setTitle({title: sender.tab.title});
+function play(args, sender) {
+  ppapLog("play");
+  targetTabId = sender.tab.id;
+  chrome.browserAction.setIcon({path: iconPathNowPlaying});
+  chrome.browserAction.setTitle({title: sender.tab.title});
 }
 
 function pause(args, sender) {
-    ppapLog("pause");
-    targetTabId = sender.tab.id;
-    chrome.browserAction.setIcon({path: iconPathNowPaused});
-    chrome.browserAction.setTitle({title: sender.tab.title});
+  ppapLog("pause");
+  targetTabId = sender.tab.id;
+  chrome.browserAction.setIcon({path: iconPathNowPaused});
+  chrome.browserAction.setTitle({title: sender.tab.title});
 }
